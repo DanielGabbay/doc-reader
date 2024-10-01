025cb11131aa633a47803bc971947d8fc57ebb3e
Daniel <daniel.g@sps.co.il>
2024-10-01 12:37:09 +0300

    init repo

--- apps/doc-reader-app/webpack.config.ts
+++ apps/doc-reader-app/webpack.config.ts
@@ -1,22 +1,9 @@
-// import { withModuleFederation } from '@nx/angular/module-federation';
-//
-// /**
-//  * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
-//  * The DTS Plugin can be enabled by setting dts: true
-//  * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
-//  */
-// export default withModuleFederation(config, {
-//   dts: false,
-//   getPublicPath: 'da',
-// });
+import { withModuleFederation } from '@nx/angular/module-federation';
+import config from './module-federation.config';
 
-export default async function (wco) {
-  // console.log('doc-reader-app: ', config);
-  // throw "Error: Module Federation is not yet supported in Nx Workspaces";
-  // const wmf = await withModuleFederation(config, {});
-  console.log('doc-reader-app: ', wco);
-  return wco || {};
-}
-
-// to remove all tmp/nx-* files
-// rm -rf tmp/nx-*
+export default {
+  ...withModuleFederation(config, {
+    dts: false,
+    runtimePlugins: [],
+  }),
+};
--- apps/doc-reader-container/webpack.config.ts
+++ apps/doc-reader-container/webpack.config.ts
@@ -1,20 +1,9 @@
-//
-// /**
-//  * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
-//  * The DTS Plugin can be enabled by setting dts: true
-//  * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
-//  */
-// // export default withModuleFederation(config, {
-// //   dts: false,
-// //   getPublicPath: 'da',
-// // })
+import { withModuleFederation } from '@nx/angular/module-federation';
+import config from './module-federation.config';
 
-export default async function (wco) {
-  // console.log('doc-reader-container: ', config);
-  // throw "Error: Module Federation is not yet supported in Nx Workspaces";
-  // const wmf = await withModuleFederation(config, {});
-  // return wmf({ ...wco, moduleFederation: true });
-  // return { ...wco, moduleFederation: true };
-  console.log('doc-reader-container: ', wco);
-  return wco || {};
-}
+export default {
+  ...withModuleFederation(config, {
+    dts: false,
+    runtimePlugins: [],
+  }),
+};
